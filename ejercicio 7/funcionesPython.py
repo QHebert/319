3856f2c2753454c42d1b8d2199733c9cca7b59c7
QHebert <hebertfqc81@hotmail.com>
2022-10-07 12:38:03 -0400

    imagen
    imagen olvidada

--- ejercicio 7/funcionesPython.py
+++ ejercicio 7/funcionesPython.py
@@ -1,4 +1,6 @@
+import os
 from tokenize import Double
+
 def suma(a, b):
   print("suma " + a + b)
 def resta(a, b):
@@ -9,8 +11,11 @@ def division(a, b):
   print("suma " + a + b)
 
 def main():
-    a = int(input('ingrese a '))
-    b = int(input('ingrese b '))
+    a = int(input("ingrese a "))
+    b = int(input("ingrese b "))
     suma(a,b)
+    resta(a,b)
+    multiplicasion(a,b)
+    division(a,b)
 if __name__ == "__main__":
     main()
